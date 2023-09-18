901d1e27fb38f5927e3f7d06a47d9b8a0aaec2ad
unkn0w_ind <100840597+unkn0wIND@users.noreply.github.com>
2023-09-18 10:58:41 +0200

    Update Avatar.tsx

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -30,6 +30,7 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
         ${isLarge ? 'w-32' : 'w-12'}
         rounded-full
         hover:opacity-90
+        transition
         cursor-pointer
         relative
         `}>
